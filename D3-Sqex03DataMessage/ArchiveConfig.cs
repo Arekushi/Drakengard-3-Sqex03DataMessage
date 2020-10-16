3f4477347503877fdeb587f94043d47f42cd60e3
Lê Hiếu <51288927+lehieugch68@users.noreply.github.com>
2020-10-16 15:38:36 +0700

    Global variables

--- D3-Sqex03DataMessage/ArchiveConfig.cs
+++ D3-Sqex03DataMessage/ArchiveConfig.cs
@@ -5,26 +5,24 @@ namespace D3_Sqex03DataMessage
 {
     public static class ArchiveConfig
     {
-        public static Dictionary<string, string> Replace { get; set; }
-
-        public static string TOCName = "PS3TOC.TXT";
-        public static string[] ArchiveName = new string[] { "ALLMESSAGE_SF.XXX", "MISSIONMESSAGE_SF.XXX" };
-        public static UInt32 Signature = 2653586369;
-        public static UInt32 UncompressedFlags = 2156396553;
-        public static byte[] UncompressedFlagsBytes = new byte[] { 0x80, 0x88, 0x00, 0x09 };
-        public static long CompressionTypeOffset = 109;
-        public static long PackageFlagsOffset = 21;
-        public static long TableOffset = 25;
-        public static long TableToData = 456;
-        public static UInt32[] Skip = new UInt32[] { 7, 43, 54 };
-        public static string[] OriginalChars = new string[]
+        public static readonly string TOCName = "PS3TOC.TXT";
+        public static readonly string[] ArchiveName = new string[] { "ALLMESSAGE_SF.XXX", "MISSIONMESSAGE_SF.XXX" };
+        public static readonly UInt32 Signature = 2653586369;
+        public static readonly UInt32 UncompressedFlags = 2156396553;
+        public static readonly byte[] UncompressedFlagsBytes = new byte[] { 0x80, 0x88, 0x00, 0x09 };
+        public static readonly long CompressionTypeOffset = 109;
+        public static readonly long PackageFlagsOffset = 21;
+        public static readonly long TableOffset = 25;
+        public static readonly long TableToData = 456;
+        public static readonly UInt32[] Skip = new UInt32[] { 7, 43, 54 };
+        public static readonly string[] OriginalChars = new string[]
         {
             '\u0001'.ToString(), '\u0003'.ToString(), '\u0004'.ToString(), '\u0005'.ToString(),
             '\u0006'.ToString(), '\u0008'.ToString(), '\u0009'.ToString(), '\u0010'.ToString(),
             '\u000a'.ToString(), '\u000b'.ToString(), '\u000c'.ToString(), '\u000d'.ToString(),
             '\u000f'.ToString(), '\u25b2'.ToString(), '\u25bc'.ToString()
         };
-        public static string[] ReplaceChars = new string[]
+        public static readonly string[] ReplaceChars = new string[]
         {
             "{01}", "{03}", "{04}", "{05}", "{06}", "{08}", "{09}", "{10}", "{0A}", "{0B}", "{0C}",
             "{0D}", "{0F}", "{25B2}", "{25BC}"
